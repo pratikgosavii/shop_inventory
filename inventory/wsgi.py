daf6898fc713b31c1d55c12bf0c5f72a51251be1
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2024-06-26 23:12:41 +0530

    Sds

--- inventory/wsgi.py
+++ inventory/wsgi.py
@@ -11,19 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dotenv import load_dotenv
-from pathlib import Path
-
-import os
-from django.core.wsgi import get_wsgi_application
-from pathlib import Path
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory.settings')
 
 application = get_wsgi_application()
