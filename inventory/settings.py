e19b3e77edc7ad3739dca094754d93964c49a25b
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2024-06-26 23:23:07 +0530

    Sds

--- inventory/settings.py
+++ inventory/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
