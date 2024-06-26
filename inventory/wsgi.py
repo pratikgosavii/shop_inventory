75c967819aa79235b50270b1915d38f4829a559a
Pratik Gosavi <pratikgosavi654@gmail.com>
2024-06-26 17:33:19 +0000

    sdsD

--- inventory/wsgi.py
+++ inventory/wsgi.py
@@ -1,16 +1,20 @@
-"""
-WSGI config for inventory project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# assuming your Django settings file is at
+# '/home/myusername/mysite/mysite/settings.py'
+path = '/home/shopinventory/shop_inventory'
+if path not in sys.path:
+     sys.path.insert(0, path)
+
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'inventory.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory.settings')
-
 application = get_wsgi_application()
