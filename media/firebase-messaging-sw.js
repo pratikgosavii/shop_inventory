6310559f0e3033c477a3a5d4b2ed944b9f63876f
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2024-07-05 17:43:15 +0530

    sdsd

--- media/firebase-messaging-sw.js
+++ media/firebase-messaging-sw.js
@@ -1,8 +1,25 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./firebase-messaging-sw.js')
-        .then(function(registration) {
-            console.log('Registration successful, scope is:', registration.scope);
-        }).catch(function(err) {
-            console.log('Service worker registration failed, error:', err);
-        });
-}
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBqaqW0SMq8SPHCWX9QKpShGValpYyV2bs",
+    authDomain: "namasterapidtax-a92e7.firebaseapp.com",
+    projectId: "namasterapidtax-a92e7",
+    storageBucket: "namasterapidtax-a92e7.appspot.com",
+    messagingSenderId: "1072169977465",
+    appId: "1:1072169977465:web:7b63186ed7f120943b76a8",
+    measurementId: "G-VTWK2QSFXM"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
