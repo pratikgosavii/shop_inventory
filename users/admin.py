e5befac913c972d214d571eb8fad463c2ab4e7dc
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-12-01 15:12:43 +0200

    dfdf

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import AdminPasswordChangeForm
 from .models import User
-from .forms import CustomUserCreationForm  # Import your custom form
+from .forms import CustomUserCreationForm, CustomUserChangeForm  # Import your custom forms
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm  # Use the custom form for user creation
+    form = CustomUserChangeForm  # Use the custom change form
+    change_password_form = AdminPasswordChangeForm  # Ensure password change form is available
     list_display = ('username', 'email', 'is_staff', 'is_superuser', 'is_accounts', 'is_admin', 'is_reception', 'is_designer', 'is_cutter', 'is_salesman', 'is_planner', 'is_executor', 'is_order_booker')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     fieldsets = (
--- users/forms.py
+++ users/forms.py
@@ -14,10 +14,35 @@ class LoginForm(forms.Form):
 
 
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User  # Import your User model
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2', 'is_accounts', 'is_admin', 'is_reception', 'is_designer', 'is_cutter')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'is_accounts',
+            'is_admin',
+            'is_reception',
+            'is_designer',
+            'is_cutter',
+            'is_salesman',
+            'is_planner',
+            'is_executor',
+            'is_order_booker',
+            'groups',
+            'user_permissions',
+        )
